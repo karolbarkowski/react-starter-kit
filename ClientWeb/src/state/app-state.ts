43fc38c366cf2f0ad8d9dbfb6a4fc851c91d710d
KarolBarkowski <karol.barkowski@gmail.com>
2021-04-08 00:52:38 +0200

    formatting fix

--- ClientWeb/src/state/app-state.ts
+++ ClientWeb/src/state/app-state.ts
@@ -1,23 +1,20 @@
-import { createState } from '@hookstate/core';
+import { createState } from '@hookstate/core'
 import { DevTools } from '@hookstate/core'
 
 const appState = createState({
-    user: {
-        login: 'LoginString',
-        email: 'admin@example.com'
-    },
-    groups: [
-        { members: 2 },
-        { members: 3 }
+  user: {
+    login: 'LoginString',
+    email: 'admin@example.com',
+  },
+  groups: [{ members: 2 }, { members: 3 }],
+  tasks: {
+    today: [
+      { id: 1, name: 'First Task' },
+      { id: 2, name: 'Second Task' },
+      { id: 3, name: 'Third Task' },
     ],
-    tasks: {
-        today: [
-            { id: 1, name: 'First Task' },
-            { id: 2, name: 'Second Task' },
-            { id: 3, name: 'Third Task' },
-        ]
-    }
-});
-DevTools(appState).label('global-app-state');
+  },
+})
+DevTools(appState).label('global-app-state')
 
-export default appState;
+export default appState
